3e9d8f7ecac8e0485da8aa4d02326986b7be0e41
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-09-27 19:43:21 +0200

    Serial és yaml példakódok

---
+++ Serial/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialProgram
+{
+    internal sealed class SerialWaiter : IDisposable
+    {
+        private readonly StringBuilder _buffer;
+        private readonly SerialPort _port;
+
+        public SerialWaiter(SerialPort port)
+        {
+            _buffer = new StringBuilder();
+            _port = port;
+            _port.DataReceived += PortDataRecieved;
+        }
+
+        private void PortDataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            _buffer.Append(_port.ReadExisting());
+        }
+
+        public async Task<string> ReadLineAsync()
+        {
+            int waited = 0;
+            while (waited < _port.ReadTimeout)
+            {
+                if (_buffer.Length > 0
+                    && _buffer[_buffer.Length - 1] == '\n')
+                {
+                    break;
+                }
+                waited++;
+                await Task.Delay(1);
+            }
+            return _buffer.ToString();
+        }
+
+        public void Dispose()
+        {
+            _port.DataReceived -= PortDataRecieved;
+        }
+    }
+
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Elérhető portok:");
+            foreach (var port in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(port);
+            }
+            Console.Write("Használni kívánt port neve: ");
+            string? selectedPortName = Console.ReadLine();
+
+            using (SerialPort serialPort = CreateSerialForAduino(selectedPortName, 115200))
+            {
+                serialPort.Open();
+
+                using (var waiter = new SerialWaiter(serialPort))
+                {
+                    serialPort.Write("hello");
+                    var response = await waiter.ReadLineAsync();
+                    Console.WriteLine(response);
+                }
+            }
+        }
+
+        private static SerialPort CreateSerialForAduino(string? portName, int baudRate)
+        {
+            if (string.IsNullOrEmpty(portName))
+                throw new ArgumentException("Nem megfelelő soros port");
+
+            return new SerialPort
+            {
+                PortName = portName,
+                BaudRate = baudRate,
+                WriteTimeout = 1000,
+                ReadTimeout = 1000,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Parity = Parity.,
+                RtsEnable = false,
+                DtrEnable = false,
+            };
+        }
+    }
+}
---
+++ Yaml/Program.cs
@@ -0,0 +1,61 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YAML
+{
+    //record a ToString() generálása miatt
+    internal record class Person
+    {
+        public string Name { get; init; }
+        public int Age { get; init; }
+        public float HeightInInches { get; init; }
+        public Address Address { get; init; }
+    }
+
+    //record a ToString() generálása miatt
+    internal record class Address
+    {
+        public string Street { get; init; }
+        public string City { get; init; }
+        public string State { get; init; }
+        public string Zip { get; init; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                Name = "Abe Lincoln",
+                Age = 25,
+                HeightInInches = 6f + 4f / 12f,
+                Address =new Address
+                {
+                    Street = "2720  Sundown Lane",
+                    City = "Kentucketsville",
+                    State = "Calousiyorkida",
+                    Zip = "99978",
+                },
+            };
+
+            //szerializáció
+            ISerializer serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            string yaml = serializer.Serialize(person);
+            
+            Console.WriteLine(yaml);
+
+            //deszerializáció
+            IDeserializer deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            Person deserizalized = deserializer.Deserialize<Person>(yaml);
+
+            Console.WriteLine(deserizalized.Name);
+        }
+    }
+}
